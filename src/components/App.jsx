1331438ff98fca6f19cfc024b87d5ccb8d5dbcb7
mays21 <mayu6_6@hotmail.com>
2024-06-02 09:04:08 +0900

    create static page

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+
+function App() {
+    return (
+        <div>
+            <Heading />
+            <Note />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
